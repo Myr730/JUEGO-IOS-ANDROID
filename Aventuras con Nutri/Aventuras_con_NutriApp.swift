bfd1dbd1707480f44a58fad846aa4f55b255ac6b
Victoria <a01734739@tec.mx>
2025-09-21 14:07:44 -0600

    Initial Commit

---
+++ Aventuras con Nutri/Aventuras_con_NutriApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aventuras_con_NutriApp.swift
+//  Aventuras con Nutri
+//
+//  Created by Victoria García on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Aventuras_con_NutriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
